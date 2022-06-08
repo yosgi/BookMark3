a1644d5414bc39184ce5e067e0f2a7f11850e172
DESKTOP-8MS7RMD\yosgi <1332729872@qq.com>
2022-06-08 22:33:11 +0800

    [update] add tailwind to project

---
+++ pages/components/header.tsx
@@ -0,0 +1,9 @@
+// create a common header component
+import React from 'react';
+export const AccoutBar:React.FC = () => {
+    return (
+        <div>
+            <h1>Account Bar</h1>
+        </div>
+    )
+}
